d381690d7da21de9085c24c844efe1373a9194ac
[Scremed] <[louispekandi@gmail.com]>
2025-02-04 10:59:06 +0700

    removing redundant code

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -11,17 +11,3 @@ class Buyer extends Model
         return $this->belongsToMany(Book::class, 'book_buyer', 'buyer_id', 'book_id');
     }
 }
-
-<?php
-
-namespace App\Models;
-
-use App\Models\Book;
-use Illuminate\Database\Eloquent\Model;
-
-class Buyer extends Model
-{
-    public function books() {
-        return $this->belongsToMany(Book::class, 'book_buyer', 'buyer_id', 'book_id',);
-    }
-}
